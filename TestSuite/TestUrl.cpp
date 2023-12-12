89b124b8e6574e04ae3b3f671d89018e40db38ae
AlgoRythm-Dylan <dperunko@comcast.net>
2023-12-12 13:01:07 -0500

    Add unit testing project

---
+++ TestSuite/TestUrl.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestSuite
+{
+	TEST_CLASS(TestUrl)
+	{
+	public:
+		
+		TEST_METHOD(ExampleUnitTest)
+		{
+			Assert::AreEqual(1, 1);
+		}
+	};
+}
