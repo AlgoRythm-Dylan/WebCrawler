a5c1dd6fbd9e0b4324f4e93990a6f0fd658a102f
AlgoRythm-Dylan <dperunko@comcast.net>
2024-02-16 15:21:51 -0500

    Add TokenBufferedLexer to clean up some syntax!

---
+++ StateMachine/TokenBufferedLexer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <queue>
+#include <string_view>
+
+#include "Lexer.h"
+#include "StringToken.h"
+
+using std::queue;
+using std::string_view;
+
+class TokenBufferedLexer : public Lexer
+{
+public:
+	queue<unique_ptr<Token>> token_buffer;
+
+	template<typename T, typename... Args>
+	void add_token(Args&&... args)
+	{
+		static_assert(std::is_base_of<Token, T>::value, "Argument must be Token");
+		token_buffer.push(unique_ptr<Token>(new T(std::forward<Args>(args)...));
+	}
+};
